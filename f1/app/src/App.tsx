a005fa23abbabb55d83d00b706c922f253541c39
Matt Pike <matt.pike@nepton.fi>
2021-09-12 21:38:52 +0100

    f1 team calculator

---
+++ f1/app/src/App.tsx
@@ -0,0 +1,173 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import {driverRates} from "./driver-rates";
+import {constructorRates} from "./constructor-rates";
+
+const COL_CONSTRUCTOR_PRICE = 1;
+const COL_CONSTRUCTOR_POINTS = 3;
+const COL_DRIVER_PRICE = 2;
+const COL_DRIVER_NAME = 0;
+const COL_DRIVER_TEAM = 1;
+const COL_DRIVER_POINTS = 4;
+const COL_SQUAD_SCORE = 0;
+const COL_SQUAD_CONSTRUCTOR = 1;
+const COL_SQUAD_DRIVER1 = 2;
+const COL_SQUAD_DRIVER2 = 3;
+const COL_SQUAD_DRIVER3 = 4;
+const COL_SQUAD_DRIVER4 = 5;
+const COL_SQUAD_DRIVER5 = 6;
+const COL_SQUAD_BUDGET_REMAINING = 7;
+
+function App() {
+    const [budget, setBudget] = useState('106.60');
+    const [bestTeams, setBestTeams] = useState<(string|number)[][]>([]);
+    const [isCalculating, setIsCalculating] = useState(false);
+
+    useEffect(()=>{
+        console.log('Budget changed and calculating: '+budget);
+        setIsCalculating(true);
+        calculateBestTeams(budget, driverRates, constructorRates).then(
+            (bestSquads: (string | number)[][]) => {
+                console.log('Calculations finished, found: '+bestSquads.length);
+                setBestTeams(bestSquads);
+                setIsCalculating(false);
+            },
+            (error) => console.error(error)
+        );
+
+    }, [budget]);
+
+console.log(bestTeams);
+    return (
+        <div className="App">
+            <label>Enter budget: $<input id={'budget'} value={budget} type={'number'} step={'0.01'}
+                    onChange={(e)=>setBudget(e.target.value)} /></label>
+            <h2>Best squads</h2>
+            <table>
+                <thead>
+                <tr>
+                    <th>Score</th>
+                    <th>Team</th>
+                    <th>Driver 1</th>
+                    <th>Driver 2</th>
+                    <th>Driver 3</th>
+                    <th>Driver 4</th>
+                    <th>Driver 5</th>
+                    <th>Remaining budget</th>
+                </tr>
+                </thead>
+                <tbody>
+                {isCalculating
+                    ? <tr><td colSpan={8}>Calculating teams...</td></tr>
+                    : bestTeams.map(row => <tr>
+                        {row.map(data => <td>{data}</td>)}
+                    </tr>)}
+                </tbody>
+            </table>
+            <h2>Drivers</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Driver</th>
+                        <th>Team</th>
+                        <th>Current price</th>
+                        <th>Season start price</th>
+                        <th>Total points</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {driverRates.map(row => <tr>
+                    {row.map(data => <td>{data}</td>)}
+                </tr>)}
+
+                </tbody>
+            </table>
+            <h2>Constructors</h2>
+            <table>
+                <thead>
+                <tr>
+                    <th>Team</th>
+                    <th>Current price</th>
+                    <th>Season start price</th>
+                    <th>Total points</th>
+                </tr>
+                </thead>
+                <tbody>
+                {constructorRates.map(row => <tr>
+                    {row.map(data => <td>{data}</td>)}
+                </tr>)}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+async function calculateBestTeams(budget:(string|number), driverRates: string[][], constructorRates: string[][]){
+    budget = parsePrice(budget+'');
+    const squads:(string|number)[][] = [];
+    //let remainingDrivers = [...driverRates];
+    for(let constructor of constructorRates) {
+        let driverCount = driverRates.length;
+        let d1, d2, d3, d4, d5;
+
+        for (d1 = 0; d1 < driverCount; d1++) {
+            for (d2 = d1 + 1; d2 < driverCount; d2++) {
+                for (d3 = d2 + 1; d3 < driverCount; d3++) {
+                    for (d4 = d3 + 1; d4 < driverCount; d4++) {
+                        for (d5 = d4 + 1; d5 < driverCount; d5++) {
+                            let cost = parsePrice(driverRates[d1][COL_DRIVER_PRICE])
+                                + parsePrice(driverRates[d2][COL_DRIVER_PRICE])
+                                + parsePrice(driverRates[d3][COL_DRIVER_PRICE])
+                                + parsePrice(driverRates[d4][COL_DRIVER_PRICE])
+                                + parsePrice(driverRates[d5][COL_DRIVER_PRICE])
+                                + parsePrice(constructor[COL_CONSTRUCTOR_PRICE]);
+                            if (cost <= budget) {
+                                let score = parseFloat(driverRates[d1][COL_DRIVER_POINTS])
+                                    + parseFloat(driverRates[d2][COL_DRIVER_POINTS])
+                                    + parseFloat(driverRates[d3][COL_DRIVER_POINTS])
+                                    + parseFloat(driverRates[d4][COL_DRIVER_POINTS])
+                                    + parseFloat(driverRates[d5][COL_DRIVER_POINTS])
+                                    + parseFloat(constructor[COL_CONSTRUCTOR_POINTS])
+                                ;
+                                squads.push([
+                                    round(score),
+                                    constructor[0],
+                                    driverRates[d1][COL_DRIVER_NAME],
+                                    driverRates[d2][COL_DRIVER_NAME],
+                                    driverRates[d3][COL_DRIVER_NAME],
+                                    driverRates[d4][COL_DRIVER_NAME],
+                                    driverRates[d5][COL_DRIVER_NAME],
+                                    round(budget - cost)
+                                ]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return squads.sort((a:(string|number)[], b:(string|number)[]):number => {
+        // @ts-ignore
+        if(Math.fround(a[0])==Math.fround(b[0])){
+            return 0;
+        }
+        // @ts-ignore
+        return Math.fround(a[0]) < Math.fround(b[0]) ? 1 : -1;
+    }).slice(0, 50);
+}
+
+function parsePrice(price: string){
+    if(price.startsWith('$')){
+        price = price.substr(1);
+    }
+    if(price.endsWith('m')){
+        price = price.substr(0, price.length-1);
+    }
+    return parseFloat(price);
+}
+
+function round(num:number):number{
+    return Math.round((num + Number.EPSILON) * 100) / 100;
+}
+
+export default App;
---
+++ f1/app/src/driver-rates.ts
@@ -0,0 +1,83 @@
+export const driverRates = [
+
+    ['Lewis Hamilton',
+        'Mercedes',
+        '$33.0m', '$33.5m', '487.5'],
+
+
+    ['Valtteri Bottas',
+        'Mercedes',
+        '$23.0m', '$23.6m', '311.5'],
+
+
+    ['Max Verstappen',
+        'Red Bull',
+        '$25.7m', '$11.7m', '485'],
+    ['Sergio Perez',
+        'Red Bull',
+        '$18.4m', '$18.4m', '295.5'],
+
+    ['Lando Norris',
+        'Mclaren',
+        '$14.2m', '$11.7m', '371.5'],
+
+    ['Daniel Ricciardo',
+        'Mclaren',
+        '$15.7m', '$17.3m', '246'],
+
+        ['Charles Leclerc',
+            'Ferrari',
+            '$17.7m', '$16.8m', '267'],
+
+        ['Carlos Sainz',
+            'Ferrari',
+            '$14.5m', '$14.4m', '256'],
+
+        ['Pierre Gasly',
+            'Alpha Tauri',
+            '$11.9m', '$11.7m', '207'],
+
+        ['Yuki Tsunoda',
+            'Alpha Tauri',
+            '$8.3m', '$8.8m', '58.5'],
+
+
+        ['Fernando Alonso',
+            'Alpine',
+            '$15.2m', '$15.6m', '206.5'],
+
+        ['Esteban Ocon',
+            'Alpine',
+            '$10.2m', '$10.1m', '206.5'],
+
+        ['Kimi Raikkonen',
+            'Alfa Romeo',
+            '$9.2m', '$9.6m', '113.5'],
+
+        ['Antonio Giovinazzi',
+            'Alfa Romeo',
+            '$8.0m', '$7.9m', '97'],
+        ['George Russell',
+            'Williams',
+            '$6.2m', '$6.2m', '111'],
+
+        ['Nicholas Latifi',
+            'Williams',
+            '$6.4m', '$6.5m', '81.5'],
+
+        ['Sebastian Vettel',
+            'Aston Martin',
+            '$15.3m', '$16.2m', '124'],
+
+        ['Lance Stroll',
+            'Aston Martin',
+            '$13.3m', '$13.9m', '0.49'],
+
+        ['Mick Schumacher',
+            'Haas',
+            '$5.8m', '$11.7m', '123'],
+
+        ['Nikita Mazepin',
+            'Haas',
+            '$5.3m', '$5.5m', '10.5']
+];
